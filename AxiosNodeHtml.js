f84bd87c9e9dd5d4b48ce4c7bbcb540e91d72728
SirawatVry <s6606021630061@email.kmutnb.ac.th>
2025-03-11 23:15:53 +0700

    aa

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -261,7 +261,7 @@ app.post('http://10.104.7.167:5001', async (req, res) => {
         const customer = await Customer.findOne({ where: { Customer_Phonenumber: phone } });
         if (customer) {
             console.log('Customer found:', customer.Customer_ID); // Debugging
-            return res.redirect(`/menu ?customerId=${customer.Customer_ID}`);
+            return res.redirect(`/menu?customerId=${customer.Customer_ID}`);
         }
 
         const employee = await Employees.findOne({ where: { Employees_Phonenumber: phone, Employees_Position: 'admin' } });
